353960f038ad71a6c21ab010e3953dea4cb265d1
slaastenkaa <slaastenkaa@gmail.com>
2020-02-25 23:07:17 +0400

    feat: add answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    var numb = n;
+    if (numb < 0) {
+        numb *= -1;
+        return Math.abs(numb)
+            .toString()
+            .split("")
+            .reverse("")
+            .join("");
+    } else {
+        return numb
+            .toString()
+            .split("")
+            .reverse("")
+            .join("");
+    }
+};
